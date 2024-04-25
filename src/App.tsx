f5b214b01efd9e3f493c9fabd28d09deecd85b9d
Arjun Krishna <krishnaarjun571@gmail.com>
2024-04-25 21:36:50 +0400

    Login Page Created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Login from './Components/Login/login.tsx';
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <Login/>
+      </div>
+      
+  )
+}
+
+export default App
---
+++ src/Components/Login/login.tsx
@@ -0,0 +1,34 @@
+import './Login.css';
+import { FaUser, FaLock } from "react-icons/fa";
+
+
+const LoginForm = () => {
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className='input-box'>
+                    <input type="text" placeholder='Username' required />
+                    <FaUser />
+                </div>
+                <div className='input-box'>
+                    <input type="password" placeholder='Password' required />
+                    <FaLock />
+                </div>
+                <div className='remember-forgot'>
+                    <label><input type="checkbox" />Remember Me</label>
+                    <a href="#">Forgot Password?</a>
+                </div>
+
+                <button type="submit">Log In</button>
+
+                <div className='register-link'>
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+            </form>
+            </div>
+    );
+
+};
+
+export default LoginForm;
