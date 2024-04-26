f48b9a8feba736d029a5ebcf1a07189bbac7bb24
varunmnx <varun.cheriyath@gmail.com>
2024-04-26 22:35:01 +0530

    add login form

--- src/Components/Login/login.tsx
+++ src/Components/Login/login.tsx
@@ -1,34 +1,37 @@
-import './Login.css';
+import "./Login.css";
 import { FaUser, FaLock } from "react-icons/fa";
 
-
 const LoginForm = () => {
-    return (
-        <div className='wrapper'>
-            <form action="">
-                <h1>Login</h1>
-                <div className='input-box'>
-                    <input type="text" placeholder='Username' required />
-                    <FaUser />
-                </div>
-                <div className='input-box'>
-                    <input type="password" placeholder='Password' required />
-                    <FaLock />
-                </div>
-                <div className='remember-forgot'>
-                    <label><input type="checkbox" />Remember Me</label>
-                    <a href="#">Forgot Password?</a>
-                </div>
+  return (
+    <div className="wrapper">
+      <form  className="form" action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Username" required />
+          <FaUser />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+          <FaLock />
+        </div>
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" />
+            Remember Me
+          </label>
+          <a href="#">Forgot Password?</a>
+        </div>
 
-                <button type="submit">Log In</button>
+        <button type="submit" className="login-btn">Log In</button>
 
-                <div className='register-link'>
-                    <p>Don't have an account? <a href="#">Register</a></p>
-                </div>
-            </form>
-            </div>
-    );
-
+        <div className="register-link">
+          <p>
+            Don't have an account? <a href="#">Register</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
