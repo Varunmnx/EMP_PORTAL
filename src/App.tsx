e2e29bca427c3db1aaeb8fae3ac33695ef1454e1
varunmnx <varun.cheriyath@gmail.com>
2024-05-02 21:15:09 +0530

    add layout pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/login.tsx';
 import Register from './Components/Register/register.tsx';
-
+import { DashboardLayout } from './Components/Layouts'
+import { Home } from './Components/pages';
 
 enum AppRoutes {
   LOGIN = '/',
   REGISTER = '/register',
+  DASHBOARD = '/dashboard',
+  HOME = '/home'
 }
 function App() {
   
@@ -18,6 +21,9 @@ function App() {
     <Routes>
       <Route path={AppRoutes.LOGIN} element={<Login />} />
       <Route path={AppRoutes.REGISTER} element={<Register />} />
+      <Route path={AppRoutes.DASHBOARD} element={<DashboardLayout />}>
+          <Route index path={AppRoutes.DASHBOARD + AppRoutes.HOME} element={<Home/>} />
+      </Route>
     </Routes>
   </Router>
      
