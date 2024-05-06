aea5b4d35769949565ab6ff2f3781504aacd7a1c
varunmnx <varun.cheriyath@gmail.com>
2024-05-07 01:06:12 +0530

    add nav

--- src/Components/Layouts/DashboardLayout/DashboardLayout.tsx
+++ src/Components/Layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,27 +1,35 @@
-import { AppShell, Burger } from '@mantine/core';
+import './index.css'
+import { AppShell, Burger, Flex, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Outlet } from 'react-router-dom';
+import { FlexBox } from '../../Flexbox/FlexBox';
 
 export default function DashboardLayout() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
     <AppShell
-    header={{ height: 100 }}
+    header={{ height: 100, }}
     navbar={{
       width: 300,
       breakpoint: 'sm',
       collapsed: { mobile: !opened },
     }} 
   >
-      <AppShell.Header>
+      <AppShell.Header bg={"red"}>
         <Burger
           opened={opened}
           onClick={toggle}
           hiddenFrom="sm"
           size="sm"
         />
-        <div>Logo</div>
+          <FlexBox container alignItems='center' backgroundColor='green' justifyContent='center' width={"100%"} height={"100%"} >
+            <div  style={{width: "100%", height: "calc(100%-10px)",backgroundColor: "blue" }}>
+               <Text size="lg">Logo</Text>
+           </div>
+        </FlexBox>
+
+
       </AppShell.Header>
 
       <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
